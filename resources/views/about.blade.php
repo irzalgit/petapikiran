6293640e0773e8396e82b2efb42f81a41ad628ad
@irzalgit <irzalpa20@gmail.com>
2021-07-06 16:50:21 +0700

    inisialisasi

---
+++ resources/views/about.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>WPU | about</title>
+</head>
+<body>
+    <h1>halaman about</h1>
+<h3> <?= $name; ?></h3>
+<h3> <?= $email; ?></h3>
+<img src="<?= $image; ?>" alt="<?= $name; ?>" width="200">
+</body>
+</html>
